c0f6d4265b6664b498aef1eaed8e47ee8037b08a
sunjinwei <sunjinwei5828@163.com>
2021-09-26 22:14:26 +0800

    实现ConfigService接口，配置service抽象类，实现公共的获取配置的逻辑，并暴露抽象方法，让子类去实现

--- apollo-configservice/src/main/java/com/ctrip/framework/apollo/configservice/service/config/AbstractConfigService.java
+++ apollo-configservice/src/main/java/com/ctrip/framework/apollo/configservice/service/config/AbstractConfigService.java
@@ -24,79 +24,82 @@ import com.ctrip.framework.apollo.core.dto.ApolloNotificationMessages;
 import com.google.common.base.Strings;
 
 import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
+ * 实现ConfigService接口，配置service抽象类，实现公共的获取配置的逻辑，并暴露抽象方法，让子类去实现
+ *
  * @author Jason Song(song_s@ctrip.com)
  */
 public abstract class AbstractConfigService implements ConfigService {
-  @Autowired
-  private GrayReleaseRulesHolder grayReleaseRulesHolder;
+    @Autowired
+    private GrayReleaseRulesHolder grayReleaseRulesHolder;
 
-  @Override
-  public Release loadConfig(String clientAppId, String clientIp, String configAppId, String configClusterName,
-      String configNamespace, String dataCenter, ApolloNotificationMessages clientMessages) {
-    // load from specified cluster first
-    if (!Objects.equals(ConfigConsts.CLUSTER_NAME_DEFAULT, configClusterName)) {
-      Release clusterRelease = findRelease(clientAppId, clientIp, configAppId, configClusterName, configNamespace,
-          clientMessages);
+    @Override
+    public Release loadConfig(String clientAppId, String clientIp, String configAppId, String configClusterName,
+                              String configNamespace, String dataCenter, ApolloNotificationMessages clientMessages) {
+        // load from specified cluster first
+        if (!Objects.equals(ConfigConsts.CLUSTER_NAME_DEFAULT, configClusterName)) {
+            Release clusterRelease = findRelease(clientAppId, clientIp, configAppId, configClusterName, configNamespace,
+                    clientMessages);
 
-      if (Objects.nonNull(clusterRelease)) {
-        return clusterRelease;
-      }
+            if (Objects.nonNull(clusterRelease)) {
+                return clusterRelease;
+            }
+        }
+
+        // try to load via data center
+        if (!Strings.isNullOrEmpty(dataCenter) && !Objects.equals(dataCenter, configClusterName)) {
+            Release dataCenterRelease = findRelease(clientAppId, clientIp, configAppId, dataCenter, configNamespace,
+                    clientMessages);
+            if (Objects.nonNull(dataCenterRelease)) {
+                return dataCenterRelease;
+            }
+        }
+
+        // fallback to default release
+        return findRelease(clientAppId, clientIp, configAppId, ConfigConsts.CLUSTER_NAME_DEFAULT, configNamespace,
+                clientMessages);
     }
 
-    // try to load via data center
-    if (!Strings.isNullOrEmpty(dataCenter) && !Objects.equals(dataCenter, configClusterName)) {
-      Release dataCenterRelease = findRelease(clientAppId, clientIp, configAppId, dataCenter, configNamespace,
-          clientMessages);
-      if (Objects.nonNull(dataCenterRelease)) {
-        return dataCenterRelease;
-      }
+    /**
+     * Find release
+     *
+     * @param clientAppId       the client's app id
+     * @param clientIp          the client ip
+     * @param configAppId       the requested config's app id
+     * @param configClusterName the requested config's cluster name
+     * @param configNamespace   the requested config's namespace name
+     * @param clientMessages    the messages received in client side
+     * @return the release
+     */
+    private Release findRelease(String clientAppId, String clientIp, String configAppId, String configClusterName,
+                                String configNamespace, ApolloNotificationMessages clientMessages) {
+        Long grayReleaseId = grayReleaseRulesHolder.findReleaseIdFromGrayReleaseRule(clientAppId, clientIp, configAppId,
+                configClusterName, configNamespace);
+
+        Release release = null;
+
+        if (grayReleaseId != null) {
+            release = findActiveOne(grayReleaseId, clientMessages);
+        }
+
+        if (release == null) {
+            release = findLatestActiveRelease(configAppId, configClusterName, configNamespace, clientMessages);
+        }
+
+        return release;
     }
 
-    // fallback to default release
-    return findRelease(clientAppId, clientIp, configAppId, ConfigConsts.CLUSTER_NAME_DEFAULT, configNamespace,
-        clientMessages);
-  }
+    /**
+     * Find active release by id
+     */
+    protected abstract Release findActiveOne(long id, ApolloNotificationMessages clientMessages);
 
-  /**
-   * Find release
-   * 
-   * @param clientAppId the client's app id
-   * @param clientIp the client ip
-   * @param configAppId the requested config's app id
-   * @param configClusterName the requested config's cluster name
-   * @param configNamespace the requested config's namespace name
-   * @param clientMessages the messages received in client side
-   * @return the release
-   */
-  private Release findRelease(String clientAppId, String clientIp, String configAppId, String configClusterName,
-      String configNamespace, ApolloNotificationMessages clientMessages) {
-    Long grayReleaseId = grayReleaseRulesHolder.findReleaseIdFromGrayReleaseRule(clientAppId, clientIp, configAppId,
-        configClusterName, configNamespace);
-
-    Release release = null;
-
-    if (grayReleaseId != null) {
-      release = findActiveOne(grayReleaseId, clientMessages);
-    }
-
-    if (release == null) {
-      release = findLatestActiveRelease(configAppId, configClusterName, configNamespace, clientMessages);
-    }
-
-    return release;
-  }
-
-  /**
-   * Find active release by id
-   */
-  protected abstract Release findActiveOne(long id, ApolloNotificationMessages clientMessages);
-
-  /**
-   * Find active release by app id, cluster name and namespace name
-   */
-  protected abstract Release findLatestActiveRelease(String configAppId, String configClusterName,
-      String configNamespaceName, ApolloNotificationMessages clientMessages);
+    /**
+     * Find active release by app id, cluster name and namespace name
+     */
+    protected abstract Release findLatestActiveRelease(String configAppId, String configClusterName,
+                                                       String configNamespaceName, ApolloNotificationMessages clientMessages);
 }
